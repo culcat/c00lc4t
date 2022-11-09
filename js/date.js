e2fc66085bc4952b6c70fab9af5280b2a14b0f14
Alexandr Krainukov <krainukovaleksandr1@gmail.com>
2022-11-09 19:13:42 +0300

    111

--- js/date.js
+++
@@ -1,6 +0,0 @@
-const date = '2022-11-07T00:00:00.000Z'
-let currentDate = Date.parse(new Date());
-let days = (currentDate - Date.parse(date));      
-console.log(Math.floor(days/8.64e7))
-
-document.querySelector('.dateFrom').innerHTML = 'Прошло ' + Math.floor(days/8.64e7) + ' дня с момента обиды Дозоровой' 
