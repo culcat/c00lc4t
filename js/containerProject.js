43c546af524a7d679135a471485e2c40f28965cf
Alexandr Krainukov <krainukovaleksandr1@gmail.com>
2022-11-12 12:04:49 +0300

    add header

--- js/containerProject.js
+++ js/containerProject.js
@@ -66,3 +66,4 @@ if (ghsClose) {
       document.querySelector('.ghsWeb').innerHTML = '' 
     }
   )  }
+
--- js/header.js
+++ js/header.js
@@ -1,17 +1,22 @@
-// const header = document.querySelector('.header')
-// header.style.width = ''
-// header.style.height = ''
-// header.style.borderRadius = ''
-// header.style.background = ''
+const header = document.querySelector('.header')
+    header.style.width = 'auto'
+    header.style.height = '4em'
+    header.style.borderRadius = '15px'
+    header.style.background = 'grey'
 
-const headerAllPage = document.querySelector('button')
+
+
+const headerAllPage = document.querySelector('.btn-1')
 if (headerAllPage) {
   headerAllPage.addEventListener('click', () => {
-    const header = document.querySelector('.header')
+    document.querySelector('.titlePages').innerHTML = 'Обо мне'
+    document.querySelector('.titlePages').style.paddingTop = '8px'
 
-    header.style.width = 'auto'
-header.style.height = '5em'
-header.style.borderRadius = '15px'
-header.style.background = 'grey'
-  
+})}
+
+const headerProjectPage = document.querySelector('.btn-2')
+if (headerProjectPage) {
+  headerProjectPage.addEventListener('click', () => {
+   document.querySelector('.titlePages').innerHTML = 'Мои проекты'
+    document.querySelector('.titlePages').style.paddingTop = '8px'
 })}
