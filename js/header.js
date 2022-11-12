4f6221f95e77ec16f1604a4e8c84a2b47f2afaa6
Alexandr Krainukov <krainukovaleksandr1@gmail.com>
2022-11-12 21:37:08 +0300

    fixed bug`s

--- js/header.js
+++ js/header.js
@@ -1,22 +1,15 @@
+document.querySelector('.titlePages').innerHTML = 'Обо мне'
+    document.querySelector('.titlePages').style.paddingTop = '1%'
+
+    document.querySelector('.titlePages1').innerHTML = 'Мои проекты'
+    document.querySelector('.titlePages1').style.paddingTop = '1%'
+
 const header = document.querySelector('.header')
     header.style.width = 'auto'
     header.style.height = '4em'
     header.style.borderRadius = '15px'
-    header.style.background = 'grey'
+    header.style.border = 'solid grey 2px '
+    header.style.background = 'black'
 
 
 
-const headerAllPage = document.querySelector('.btn-1')
-if (headerAllPage) {
-  headerAllPage.addEventListener('click', () => {
-    document.querySelector('.titlePages').innerHTML = 'Обо мне'
-    document.querySelector('.titlePages').style.paddingTop = '8px'
-
-})}
-
-const headerProjectPage = document.querySelector('.btn-2')
-if (headerProjectPage) {
-  headerProjectPage.addEventListener('click', () => {
-   document.querySelector('.titlePages').innerHTML = 'Мои проекты'
-    document.querySelector('.titlePages').style.paddingTop = '8px'
-})}
